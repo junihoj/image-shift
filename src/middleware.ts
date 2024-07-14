e4c6ccf1da67bf77211dd69cf2bc4d4afe34113c
junihoj <ezeonyekachukwu98@gmail.com>
2024-07-14 16:33:34 +0100

    added clerk webhook

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,10 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    // matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: [
+        '/((?!.*\\..*|_next|api/webhooks).*)', // Exclude static files, _next, and api/webhooks routes
+        '/',
+        '/(api|trpc)(.*)',
+    ],
 };
